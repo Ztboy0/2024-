b1ddb93c0d1216edbb41fd31c1e489953d6ec307
kehan <kehan@123.com>
2024-06-27 23:22:44 +0800

    new add

---
+++ day07_weblogx/src/main/java/cn/itcast/weblog/controller/IndexController.java
@@ -0,0 +1,28 @@
+package cn.itcast.weblog.controller;
+
+import cn.itcast.weblog.pojo.*;
+import cn.itcast.weblog.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index.action")
+    public String skipToIndex(){
+        return "index";
+    }
+
+
+    @Autowired
+    private TemperatureService tempService;
+    @RequestMapping("/getTemperature.action")
+    @ResponseBody
+    public TemperatureReturnPojo getTemperature(){
+        TemperatureReturnPojo temperaturePojo =  tempService.getAllTemperature();
+        System.out.println(temperaturePojo);
+        return temperaturePojo;
+    }
+}
